27f13f7bcc65ca5792f1e9bb5c86e62d166f4cda
mdobrychlop <mateusz.dobrychlop@gmail.com>
2017-07-31 15:28:24 +0200

    Remove Modules

--- Modules/Trans/ScoreSaver.py
+++
@@ -1,7 +0,0 @@
-class ScoreSaver():
-    def __init__(self, obj, method):
-        self.obj = obj
-        self.method = method
-    
-    def __call__(self, restraintId, score):
-        getattr(self.obj, self.method)(restraintId, score)
